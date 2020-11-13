b746b1f9c05489e8b8827047097aa628d6eda509
longdt8 <long.dinh@onemount.com>
2020-11-13 16:11:38 +0700

    release v1.0.0

--- test-functional/src/main/java/repository/RuleTemplateRepository.java
+++ test-functional/src/main/java/repository/RuleTemplateRepository.java
@@ -3,10 +3,17 @@ package repository;
 
 import com.github.longdt.vertxorm.annotation.Repository;
 import com.github.longdt.vertxorm.repository.CrudRepository;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.sqlclient.Pool;
 import model.RuleTemplate;
 import repository.impl.RuleTemplateRepositoryImpl;
 
 @Repository(extending = RuleTemplateRepositoryImpl.class)
 public interface RuleTemplateRepository extends CrudRepository<Long, RuleTemplate> {
-    void doOtherThing();
+    void doSomething(Handler<AsyncResult<Long>> resultHandler);
+
+    static RuleTemplateRepository create(Pool pool) {
+        return new RuleTemplateRepositoryFactory(pool);
+    }
 }
--- test-functional/src/main/java/repository/impl/RuleTemplateRepositoryImpl.java
+++ test-functional/src/main/java/repository/impl/RuleTemplateRepositoryImpl.java
@@ -1,13 +1,14 @@
 package repository.impl;
 
 import com.github.longdt.vertxorm.repository.postgresql.AbstractCrudRepository;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import model.RuleTemplate;
 import repository.RuleTemplateRepository;
 
 public class RuleTemplateRepositoryImpl extends AbstractCrudRepository<Long, RuleTemplate> implements RuleTemplateRepository {
-
     @Override
-    public void doOtherThing() {
+    public void doSomething(Handler<AsyncResult<Long>> resultHandler) {
 
     }
 }
